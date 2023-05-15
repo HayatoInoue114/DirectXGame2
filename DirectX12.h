46b3929f1182f410b389f397e744f921275ea8ed
k022g0087@g.neec.ac.jp <k022g0087@g.neec.ac.jp>
2023-05-15 16:20:01 +0900

    a

---
+++ DirectX12.h
@@ -0,0 +1,32 @@
+#pragma once
+#include <d3d12.h>
+#include <dxgi1_6.h>
+#include <cassert>
+#include <string.h>
+#include <format>
+
+
+#pragma comment(lib,"d3d12.lib")
+#pragma comment(lib,"dxgi.lib")
+
+class DirectX12
+{
+public:
+	DirectX12();
+	~DirectX12();
+
+	void DXGIFactory();
+
+	void Adapter();
+
+	void D3D12Device();
+
+	void Init();
+private:
+	IDXGIFactory7* dxgiFactory;
+	HRESULT hr;
+
+	//使用するアダプタ用の変数。最初にnullptrを入れておく
+	IDXGIAdapter4* useAdapter;
+};
+

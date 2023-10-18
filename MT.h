a2625a1ff011753340a761a0b116588fc6fcfbce
k022g0087@g.neec.ac.jp <k022g0087@g.neec.ac.jp>
2023-10-18 13:01:31 +0900

    a

--- MT.h
+++ MT.h
@@ -59,3 +59,10 @@ float InverseNum2(const Matrix4x4& m, int a, int b, int c, int d, int e, int f);
 
 //逆行列
 Matrix4x4 Inverse(const Matrix4x4& m);
+
+//1.透視投影行列
+Matrix4x4 MakePerspectiveFovMatrix(float fovY, float aspectRatio, float nearClip, float farClip);
+//2.正射影行列
+Matrix4x4 MakeOrthographicMatrix(float left, float right, float top, float bottom, float nearClip, float farClip);
+//3.ビューポート変換行列
+Matrix4x4 MakeViewportMatrix(float left, float top, float width, float height, float minDepth, float maxDepth);

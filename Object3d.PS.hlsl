a112989376d9bfa19c30410e1059ef0683d6b3cc
k022g0087@g.neec.ac.jp <k022g0087@g.neec.ac.jp>
2023-11-09 12:18:48 +0900

    PS

--- Object3d.PS.hlsl
+++ Object3d.PS.hlsl
@@ -27,11 +27,17 @@ PixelShaderOutput main(VertexShaderOutput input) {
 	float4 transformdUV = mul(float32_t4(input.texcoord,0.0f,1.0f),gMaterial.uvTransform);
 	float32_t4 textureColor = gTexture.Sample(gSampler, transformdUV.xy);
 
+	//textureのa値が0.5以下の時にPixelを棄却
+	if(textureColor.a <= 0.5) {
+		discard;
+	}
+
 	if (gMaterial.enableLighting != 0) {// LIghtingする場合
 		//half lambert
 		float NdotL = dot(normalize(input.normal), -gDirectionalLight.direction);
 		float cos = pow(NdotL * 0.5f + 0.5f, 2.0f);
-		output.color = gMaterial.color * textureColor * gDirectionalLight.color * cos * gDirectionalLight.intensity;
+		output.color.rgb = gMaterial.color.rgb * textureColor.rgb * gDirectionalLight.color.rgb * cos * gDirectionalLight.intensity;
+		output.color.a = gMaterial.color.a * textureColor.a;
 	}
 	else {
 		output.color = gMaterial.color * textureColor;

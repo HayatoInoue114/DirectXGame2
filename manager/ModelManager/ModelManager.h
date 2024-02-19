7a41f8b472a7309707c60e62574296c71aca8cd5
k022g0087@g.neec.ac.jp <k022g0087@g.neec.ac.jp>
2024-02-19 12:26:45 +0900

    a

--- manager/ModelManager/ModelManager.h
+++ manager/ModelManager/ModelManager.h
@@ -1,5 +1,37 @@
 #pragma once
-class ModelManager
-{
+#include "../../structure/ModelData/ModelData.h"
+#include <list>
+
+enum ModelName {
+	CUBE,
+	SPHERE,
+	SKYDOME,
+	PLAYER,
+	ENEMY,
+
+	MODELNUM
 };
 
+class ModelManager
+{
+public:
+	// コンストラクタ
+	ModelManager() = default;
+	// デストラクタ
+	~ModelManager() = default;
+
+	// シングルトン
+	static ModelManager* GetInstance();
+
+	// 初期化
+	void Initialize();
+
+	// objファイルの読み込み
+	ModelData LoadObjFile(const std::string& directoryPath, const std::string& filename);
+
+	// Getter
+	ModelData* GetModelData() { return modelData_; }
+
+private:
+	ModelData modelData_[MODELNUM];
+};

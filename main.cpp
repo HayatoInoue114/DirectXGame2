0da5fee4a951a8f7ecbe0683f81905e194d98a43
HayatoInoue114 <k022g0087@g.neec.ac.jp>
2024-02-23 03:05:02 +0900

    a

--- main.cpp
+++ main.cpp
@@ -1,36 +1,7 @@
 #include "manager/GameManager/GameManager.h"
 
 int WINAPI WinMain(HINSTANCE, HINSTANCE, LPSTR, int) {
-	CoInitializeEx(0, COINIT_MULTITHREADED);
-
-	GameManager* gameManager = new GameManager;
-
-	gameManager->VariableInit();
-
-	gameManager->Initialize();
-
-	MSG msg{};
-	//Windowにメッセージが来てたら最優先で処理させる
-	while (msg.message != WM_QUIT)
-	{
-		if (PeekMessage(&msg, NULL, 0, 0, PM_REMOVE)) {
-			TranslateMessage(&msg);
-			DispatchMessage(&msg);
-		}
-		else {
-			//ゲームの処理
-			gameManager->BeginFrame();
-
-			gameManager->Update();
-
-			gameManager->Draw();
-
-			gameManager->EndFrame();
-			
-		}
-	}
-
-	gameManager->Finalize();
-	CoUninitialize();
+	std::unique_ptr<GameManager> gameManager = std::make_unique<GameManager>();
+	gameManager->Update();
 	return 0;
 }

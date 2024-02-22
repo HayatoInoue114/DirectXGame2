136b52be499735ac965a8636a7fe9cebdc7a8467
HayatoInoue114 <k022g0087@g.neec.ac.jp>
2024-02-23 02:25:44 +0900

    a

---
+++ scene/GameClearScene.h
@@ -0,0 +1,26 @@
+#pragma once
+#include "IScene.h"
+#include "../Input/Input.h"
+#include "../object/Sprite/Sprite.h"
+
+class GameManager;
+class GameClearScene : public IScene
+{
+public:
+	// 初期化
+	void Initialize()override;
+
+	// 更新処理
+	void Update()override;
+
+	// 描画
+	void Draw()override;
+
+	void Finalize()override;
+private:
+	Input* input_{};
+
+	Sprite* sprite_{};
+
+	WorldTransform transform_{};
+};

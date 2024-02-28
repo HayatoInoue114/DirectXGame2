a792b6d7cbb8fd0158439aa6827c8f1d4fa0cc81
k022g0087@g.neec.ac.jp <k022g0087@g.neec.ac.jp>
2024-02-28 11:16:34 +0900

    a

--- object/Model/Model.cpp
+++ object/Model/Model.cpp
@@ -1,1 +1,236 @@
 #include "Model.h"
+#include <assert.h>
+
+ModelData Model::LoadObjFile(const std::string& directoryPath, const std::string& filename) {
+	ModelData modelData; // 構築するModelData
+	std::vector<Vector4> positions; // 位置
+	std::vector<Vector3> normals; // 法線
+	std::vector<Vector2> texcoords; // テクスチャ座標
+	std::string line; // ファイルから呼んだ1行を格納するもの
+
+	std::ifstream file(directoryPath + "/" + filename + ".obj"); // ファイルを開く
+	assert(file.is_open()); // とりあえず聞けなかったら止める
+
+	while (std::getline(file, line))
+	{
+		std::string identifier;
+		std::istringstream s(line);
+		s >> identifier; // 先頭の識別子を読む
+
+		//identifierに応じた報酬
+		if (identifier == "v") {
+			Vector4 position;
+			s >> position.x >> position.y >> position.z;
+			position.x *= -1.0f;
+			position.w = 1.0f;
+			positions.push_back(position);
+		}
+		else if (identifier == "vt") {
+			Vector2 texcoord;
+			s >> texcoord.x >> texcoord.y;
+			texcoord.y = 1.0f - texcoord.y;
+			texcoords.push_back(texcoord);
+		}
+		else if (identifier == "vn") {
+			Vector3 normal;
+			s >> normal.x >> normal.y >> normal.z;
+			normal.x *= -1.0f;
+			normals.push_back(normal);
+		}
+		else if (identifier == "f") {
+			VertexData triangle[3];
+			//面は三角形限定。その他は未対応
+			for (int32_t faceVertex = 0; faceVertex < 3; ++faceVertex) {
+				std::string vertexDefinition;
+				s >> vertexDefinition;
+				//頂点の要素へのIndexは「位置/UV/法線」で格納されているので、分解してIndexを取得する
+				std::istringstream v(vertexDefinition);
+				uint32_t elementIndices[3];
+				for (int32_t element = 0; element < 3; ++element) {
+					std::string index;
+					std::getline(v, index, '/');// /区切りでインデックスを読んでいく
+					elementIndices[element] = std::stoi(index);
+				}
+				Vector4 position = positions[elementIndices[0] - 1];
+				Vector2 texcoord = texcoords[elementIndices[1] - 1];
+				Vector3 normal = normals[elementIndices[2] - 1];
+				VertexData vertex = { position,texcoord,normal };
+				modelData.vertices.push_back(vertex);
+				triangle[faceVertex] = { position,texcoord,normal };
+			}
+			//頂点を逆順で登録することで、回り順を逆にする
+			modelData.vertices.push_back(triangle[2]);
+			modelData.vertices.push_back(triangle[1]);
+			modelData.vertices.push_back(triangle[0]);
+		}
+		else if (identifier == "mtllib") {
+			//materialTemplateLibraryファイルの名前を取得する
+			std::string materialFilename;
+			s >> materialFilename;
+			//基本的にobjファイルと同一階層にmtlは存在させるので,ディレクトリ名とファイル名を渡す
+			modelData.material = LoadMaterialTemplateFile(directoryPath, materialFilename);
+		}
+	}
+	return modelData;
+}
+
+Model::Model() {
+
+}
+
+Model::~Model() {
+
+}
+
+Model* Model::CreateModelFromObj(int modelName) {
+	Model* model = new Model();
+	// モデルの読み込み
+
+	model->modelData_ = ModelManager::GetInstance()->GetModelData()[modelName];
+
+	model->Initialize();
+	return model;
+}
+
+std::unique_ptr<Model> Model::CreateModelFromObjPtr(int modelName) {
+	std::unique_ptr<Model> model;
+	model = std::make_unique<Model>();
+	// モデルの読み込み
+	//model->SetTextureNum(modelName);
+	model->modelData_ = ModelManager::GetInstance()->GetModelData()[modelName];
+
+	model->Initialize();
+	return model;
+}
+
+void Model::Initialize() {
+
+	//Transform変数を作る
+	worldTransform_.Initialize();
+
+	//CreatevertexResource();
+	CreateModel();
+	CreateMaterialResource();
+	CreateVertexBufferView();
+	CreateTransformationMatrixResource();
+
+	WriteDataToResource();
+
+
+	SetMaterialData();
+}
+
+void Model::CreatevertexResource() {
+	//vertexResource_ = directX12_->CreateBufferResource(directX12_->GetDevice(), sizeof(VertexData) * vertexIndex_);
+}
+
+void Model::CreateVertexBufferView() {
+	vertexBufferView_ = {};
+	//リソースの先頭のアドレスから使う
+	vertexBufferView_.BufferLocation = vertexResource_->GetGPUVirtualAddress();
+	//使用するリソースのサイズは頂点３つ分のサイズ
+	vertexBufferView_.SizeInBytes = UINT(sizeof(VertexData) * modelData_.vertices.size());
+	//1頂点当たりのサイズ
+	vertexBufferView_.StrideInBytes = sizeof(VertexData);
+}
+
+void Model::CreateMaterialResource() {
+	materialResource_ = DirectX12::GetInstance()->CreateBufferResource(DirectX12::GetInstance()->GetDevice().Get(), sizeof(Material));
+	materialData_ = nullptr;
+	materialResource_->Map(0, nullptr, reinterpret_cast<void**>(&materialData_));
+}
+
+void Model::CreateTransformationMatrixResource() {
+	//WVP用のリソースを作る。Matrix4x4　1つ分のサイズを用意する
+	wvpResource_ = DirectX12::GetInstance()->CreateBufferResource(DirectX12::GetInstance()->GetDevice().Get(), sizeof(TransformationMatrix));
+	//データを書き込む
+	wvpData_ = nullptr;
+	//書き込むためのアドレスを取得
+	wvpResource_->Map(0, nullptr, reinterpret_cast<void**>(&wvpData_));
+	//単位行列を書き込んでおく
+	wvpData_->WVP = MakeIdentity4x4();
+	wvpData_->World = MakeIdentity4x4();
+}
+
+void Model::WriteDataToResource() {
+	//書き込むためのアドレスを取得
+	vertexResource_->Map(0, nullptr, reinterpret_cast<void**>(&vertexData_));
+	//頂点データをリソースにコピー
+	std::memcpy(vertexData_, modelData_.vertices.data(), sizeof(VertexData) * modelData_.vertices.size());
+}
+
+void Model::CreateWVPMatrix() {
+	cameraTransform_.scale_ = { 1.0f,1.0f,1.0f };
+	cameraTransform_.rotation_ = {};
+	cameraTransform_.translation_ = { 0.0f,0.0f,-10.0f, };
+
+	worldMatrix_ = MakeAffineMatrix(worldTransform_.scale_, worldTransform_.rotation_, worldTransform_.translation_);
+	cameramatrix_ = MakeAffineMatrix(cameraTransform_.scale_, cameraTransform_.rotation_, cameraTransform_.translation_);
+	viewMatrix_ = Inverse(cameramatrix_);
+	projectionMatix_ = MakePerspectiveFovMatrix(0.45f, float(kCliantWidth) / float(kCliantHeight), 0.1f, 100.0f);
+	worldViewProjectionMatrix_ = Multiply(worldMatrix_, Multiply(viewMatrix_, projectionMatix_));
+	wvpData_->WVP = worldViewProjectionMatrix_;
+	wvpData_->World = worldMatrix_;
+}
+
+void Model::SetMaterialData() {
+	// 色の設定
+	materialData_->color = { 1.0f,1.0f,1.0f,1.0f };
+	// Lightingを有効にする
+	materialData_->enableLighting = true;
+	//UVTransformを単位行列で初期化
+	materialData_->uvTransform = MakeIdentity4x4();
+	//uvTransform用の変数
+	uvTransform_.scale_ = { 1.0f,1.0f,1.0f };
+	uvTransform_.rotation_ = {};
+	uvTransform_.translation_ = {};
+}
+
+void Model::CreateModel() {
+	//モデル読み込み
+	//modelData_ = LoadObjFile("resources", "axis");
+	//頂点リソースを作る
+	vertexResource_ = DirectX12::GetInstance()->CreateBufferResource(DirectX12::GetInstance()->GetDevice().Get(), sizeof(VertexData) * modelData_.vertices.size());
+
+
+}
+
+void Model::Update() {
+	//ImGuiAdjustParameter();
+}
+
+void Model::Draw(WorldTransform& worldTransform, ViewProjection& viewProjection, uint32_t textureNum) {
+	worldTransform_ = worldTransform;
+	CreateWVPMatrix();
+	//パラメータからUVTransform用の行列を生成する
+	uvTransformMatrix_ = MakeScaleMatrix(uvTransform_.scale_);
+	uvTransformMatrix_ = Multiply(uvTransformMatrix_, MakeRotateZMatrix(uvTransform_.rotation_.z));
+	uvTransformMatrix_ = Multiply(uvTransformMatrix_, MakeTranslateMatrix(uvTransform_.translation_));
+	materialData_->uvTransform = uvTransformMatrix_;
+
+	DirectX12::GetInstance()->GetCommandList()->IASetVertexBuffers(0, 1, &vertexBufferView_);	//VBVを設定
+	//形状を設定。PSOに設定しているものとはまた別。同じものを設定すると考えておけばよい
+	DirectX12::GetInstance()->GetCommandList()->IASetPrimitiveTopology(D3D_PRIMITIVE_TOPOLOGY_TRIANGLELIST);
+	DirectX12::GetInstance()->GetCommandList()->SetGraphicsRootConstantBufferView(0, materialResource_->GetGPUVirtualAddress());
+	//wvp用のCBufferの場所を設定
+	DirectX12::GetInstance()->GetCommandList()->SetGraphicsRootConstantBufferView(1, wvpResource_->GetGPUVirtualAddress());
+	//SRV用のDescriptorTableの先頭を設定。2はrootParameter[2]である。
+	DirectX12::GetInstance()->GetCommandList()->SetGraphicsRootDescriptorTable(2, TextureManager::GetInstance()->GetTextureSrvHandleGPU()[textureNum]);
+	DirectX12::GetInstance()->GetCommandList()->SetGraphicsRootConstantBufferView(3, Light::Getinstance()->GetDirectionalLightResource()->GetGPUVirtualAddress());
+
+	//描画！　（DrawCall/ドローコール)。3頂点で1つのインスタンス。
+	DirectX12::GetInstance()->GetCommandList()->DrawInstanced(UINT(modelData_.vertices.size()), 1, 0, 0);
+}
+
+void Model::ImGuiAdjustParameter() {
+	ImGui::Text("Model");
+	ImGui::CheckboxFlags("isLighting", &materialData_->enableLighting, 1);
+	ImGui::SliderFloat3("Translate", &worldTransform_.translation_.x, -5, 5);
+	ImGui::SliderFloat3("Scale", &worldTransform_.scale_.x, -5, 5);
+	ImGui::SliderFloat3("Rotate", &worldTransform_.rotation_.x, -5, 5);
+	ImGui::Text("UVTransform");
+	ImGui::DragFloat2("UVTranslate", &uvTransform_.translation_.x, 0.01f, -10.0f, 10.0f);
+	ImGui::DragFloat2("UVScale", &uvTransform_.scale_.x, 0.01f, -10.0f, 10.0f);
+	ImGui::SliderAngle("UVRotate.z", &uvTransform_.rotation_.z);
+}
+

680775f68830f0b8fa26eeec9629c1e6c47981b2
k022g0087@g.neec.ac.jp <k022g0087@g.neec.ac.jp>
2024-04-22 12:14:38 +0900

    a

--- manager/PrimitiveManager/PrimitiveManager.cpp
+++ manager/PrimitiveManager/PrimitiveManager.cpp
@@ -3,40 +3,11 @@
 
 void PrimitiveManager::Initialize() {
 
-	railCamera_ = std::make_unique<RailCamera>();
-	railCamera_->Initialize({ 0.0f, 0.0f, 0.0f }, { 0.0f, 0.0f, 0.0f });
 
-	player_ = std::make_unique<Player>();
-	model_ = Model::CreateModelFromObjPtr(PLAYER);
-	// 自キャラとレールカメラの親子関係を結ぶ
-	model_->SetCamera(railCamera_->GetCamera());
-	model_->SetParent(&railCamera_->GetWorldTransform());
-	player_->SetCamera(railCamera_->GetCamera());
-
-	Vector3 playerPosition = {0.0f,0.0f,50.0f};
-
-	player_->Initialize(model_.get(), playerPosition);
-
-	domeModel_ = std::make_unique<Model>();
-	domeModel_ = Model::CreateModelFromObjPtr(SKYDOME);
-	domeModel_->SetCamera(railCamera_->GetCamera());
-
-	dome_ = std::make_unique<Skydome>();
-	dome_->Initialize(domeModel_.get()/*,railCamera_->GetCamera()*/);
-
-
-
-	particle_.Initialize();
 }
 
 void PrimitiveManager::Update() {	
 
-
-	railCamera_->Update();
-
-	player_->Update();
-	particle_.Update();
-	particle_.ImGuiAdjustParameter();
 }
 
 void PrimitiveManager::Draw() 
@@ -48,13 +19,11 @@ void PrimitiveManager::Draw()
 void PrimitiveManager::Draw2D()
 {
 	GraphicsRenderer::GetInstance()->SetRootSignatureAndPSO(0);
-	dome_->Draw();
-	player_->Draw();
+
 
 }
 
 void PrimitiveManager::Draw3D()
 {
 	GraphicsRenderer::GetInstance()->SetRootSignatureAndPSO(1);
-	particle_.Draw(railCamera_->GetCamera(), UVCHECKER);
 }
--- manager/PrimitiveManager/PrimitiveManager.h
+++ manager/PrimitiveManager/PrimitiveManager.h
@@ -3,11 +3,6 @@
 //#include "../../object/Primitive/PrimitiveCommon/PrimitiveCommon.h"
 #include "../../object/Camera/Camera.h"
 #include "../../object/Model/Model.h"
-#include "../../object/Particle/Particle.h"
-#include "../../Project/Skydome/Skydome.h"
-#include "../../Project/RailCamera/RailCamera.h"
-#include "../../Project/Player/Player.h"
-#include "../../Project/Enemy/Enemy.h"
 
 #include <memory>
 
@@ -28,28 +23,7 @@ private:
 
 	
 private:
-	std::unique_ptr<Primitive> primitive_;
 
-	std::unique_ptr<Model> model_;
-
-	std::unique_ptr<Primitive> primitive2_;
-
-	std::unique_ptr<Model> model2_;
-	//std::unique_ptr<PrimitiveCommon> primitiveCommon_;
-
-	std::unique_ptr<Camera> camera_;
-
-	std::unique_ptr<Skydome> dome_;
-
-	std::unique_ptr<Model> domeModel_;
-
-	std::unique_ptr<RailCamera> railCamera_;
-
-	//std::unique_ptr<Particle> particle_{};
-
-	Particle particle_;
-
-	std::unique_ptr<Player> player_;
 	
 };
 

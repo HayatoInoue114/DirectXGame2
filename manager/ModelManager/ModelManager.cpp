71ac4b63714119e91cfd624970eaff19e5cc7cef
k022g0087@g.neec.ac.jp <k022g0087@g.neec.ac.jp>
2024-05-23 11:00:46 +0900

    a

--- manager/ModelManager/ModelManager.cpp
+++ manager/ModelManager/ModelManager.cpp
@@ -222,6 +222,24 @@ ModelData ModelManager::LoadFile(const std::string& directoryPath, const std::st
 				}
 			}
 		}
+
+		for (uint32_t boneIndex = 0; boneIndex < mesh->mNumBones; ++boneIndex) {
+			aiBone* bone = mesh->mBones[boneIndex];
+			std::string jointName = bone->mName.C_Str();
+			JointWeightData& jointWeightData = modelData.skinClusterData[jointName];
+
+			aiMatrix4x4 bindPoseMatrixAssimp = bone->mOffsetMatrix.Inverse();
+			aiVector3D scale, translate;
+			aiQuaternion rotate;
+			bindPoseMatrixAssimp.Decompose(scale, rotate, translate);
+			Matrix4x4 bindposeMatrix = MakeAffineMatrix(
+				{ scale.x,scale.y,scale.z }, { rotate.x,-rotate.y,-rotate.z,rotate.w }, { -translate.x,translate.y,translate.z });
+			jointWeightData.inverseBindPoseMatrix = Inverse(bindposeMatrix);
+
+			for (uint32_t weightIndex = 0; weightIndex < bone->mNumWeights; ++weightIndex) {
+				jointWeightData.vertexWeights.push_back({ bone->mWeights[weightIndex].mWeight,bone->mWeights[weightIndex].mVertexId });
+			}
+		}
 	}
 	if (GetExtention(filePath) == ".gltf") {
 		modelData.rootNode = ReadNode(scene->mRootNode);

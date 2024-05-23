859a535d973230420c3976f2ed9be3f6f73865d9
k022g0087@g.neec.ac.jp <k022g0087@g.neec.ac.jp>
2024-05-23 11:05:44 +0900

    a

--- structure/ModelData/ModelData.h
+++ structure/ModelData/ModelData.h
@@ -3,11 +3,24 @@
 #include <vector>
 #include "../MaterialData/MaterialData.h"
 #include "../Node/Node.h"
+#include <map>
+
+struct VertexWeightData
+{
+	float weight;
+	uint32_t vertexIndex;
+};
+
+struct JointWeightData
+{
+	Matrix4x4 inverseBindPoseMatrix;
+	std::vector<VertexWeightData> vertexWeights;
+};
 
 struct ModelData
 {
+	std::map<std::string, JointWeightData> skinClusterData;
 	std::vector<VertexData> vertices;
-	std::vector<uint32_t> indices;
 	MaterialData material;
 	Node rootNode;
 };

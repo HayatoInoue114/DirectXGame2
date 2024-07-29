83f3eb5d21511d5360b5ae37de9b56843342f8c4
k022g0087@g.neec.ac.jp <k022g0087@g.neec.ac.jp>
2024-07-29 10:40:48 +0900

    a

--- ShaderFile/Object3d.PS.hlsl
+++ ShaderFile/Object3d.PS.hlsl
@@ -1,49 +1,87 @@
 #include "Object3d.hlsli"
 
-struct Material {
-	float4 color;
-	int enableLighting;
-	float4x4 uvTransform;
+struct PixelShaderOutput
+{
+    float4 color : SV_TARGET0;
+};
+
+struct Material
+{
+    float4 color;
+    int enableLighting;
+    float4x4 uvTransform;
+    int eneblePhong;
+    float shininess;
+    int enableEnvTexture;
 };
 
 struct DirectionalLight
 {
-	float4 color; //!< ライトの色
-	float3 direction; //!< ライトの向き
-	float intensity; //!< 輝度
+    float4 color;
+    float3 direction;
+    float intensity;
+};
+
+struct Camera
+{
+    float3 worldPosition;
 };
 
 ConstantBuffer<Material> gMaterial : register(b0);
+ConstantBuffer<DirectionalLight> gDirectionalLight : register(b1);
+ConstantBuffer<Camera> gCamera : register(b2);
+
 Texture2D<float4> gTexture : register(t0);
+//TextureCube<float4> gEnviromentTexture : register(t1);
+
 SamplerState gSampler : register(s0);
-ConstantBuffer<DirectionalLight> gDirectionalLight : register(b1); 
 
-struct PixelShaderOutput {
-	float4 color : SV_TARGET0;
-};
+PixelShaderOutput main(VertexShaderOutput input)
+{
+    PixelShaderOutput output;
 
-PixelShaderOutput main(VertexShaderOutput input) {
-	PixelShaderOutput output;
-	//float4 transformdUV = mul(float4(input.texcoord.x,input.texcoord.y,0.0f,1.0f),gMaterial.uvTransform);
-	float4 textureColor = gTexture.Sample(gSampler, input.texcoord);
+    float4 transformedUV = mul(float4(input.texcoord, 0.0f, 1.0f), gMaterial.uvTransform);
+    float4 textureColor = gTexture.Sample(gSampler, transformedUV.xy);
 
-	//textureのa値が0.5以下の時にPixelを棄却
-	if(textureColor.a <= 0.5) {
-		discard;
-	}
+    output.color = gMaterial.color * textureColor;
+	
+    if (gMaterial.enableLighting != 0)
+    {
+        float NdotL = dot(normalize(input.normal), -gDirectionalLight.direction);
+        float cos = pow(NdotL * 0.5f + 0.5f, 2.0f);
 
-	if (gMaterial.enableLighting != 0) {// LIghtingする場合
-		//half lambert
-		float NdotL = dot(normalize(input.normal), -gDirectionalLight.direction);
-		float cos = pow(NdotL * 0.5f + 0.5f, 2.0f);
-		//output.color.rgb = gMaterial.color.rgb * textureColor.rgb * gDirectionalLight.color.rgb * cos * gDirectionalLight.intensity;
-        output.color.rgb = textureColor.rgb;
-        //output.color.rgb = float3(1, 0, 0);
-		output.color.a = gMaterial.color.a * textureColor.a;
-	}
-	else {
-		output.color = gMaterial.color * textureColor;
-	}
+        output.color = gMaterial.color * textureColor * gDirectionalLight.color * cos * gDirectionalLight.intensity;
 
-	return output;
+        if (gMaterial.eneblePhong != 0)
+        {
+            float3 toEye = normalize(gCamera.worldPosition - input.worldPosition);
+
+            float3 reflectLight = reflect(gDirectionalLight.direction, normalize(input.normal));
+
+            float RdotE = dot(reflectLight, toEye);
+            float specularPow = pow(saturate(RdotE), gMaterial.shininess);
+
+            float3 diffuse = gMaterial.color.rgb * textureColor.rgb * gDirectionalLight.color.rgb * cos * gDirectionalLight.intensity;
+
+            float3 specular = gDirectionalLight.color.rgb * gDirectionalLight.intensity * specularPow * float3(1.0f, 1.0f, 1.0f);
+
+            output.color.rgb = diffuse + specular;
+            output.color.a = gMaterial.color.a * textureColor.a;
+        }
+    }
+    else
+    {
+        output.color = gMaterial.color * textureColor;
+    }
+	
+    if (gMaterial.enableEnvTexture != 0)
+    {
+        float3 cameraToPosition = normalize(input.worldPosition - gCamera.worldPosition);
+        float3 reflectedVector = reflect(cameraToPosition, normalize(input.normal));
+        //float4 enviromentColor = gEnviromentTexture.Sample(gSampler, reflectedVector);
+		
+        //output.color.rgb += enviromentColor.rgb;
+    }
+
+    return output;
 }
